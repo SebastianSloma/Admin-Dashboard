9a609b97ca2a22b699d187092928cb6acb7de593
Sebastian <sebastiansloma@gmail.com>
2023-02-27 18:06:25 +0100

    Added function in js: Show and close sidebar menu and dark-light mode

---
+++ orders.js
@@ -0,0 +1,36 @@
+const Orders = [
+	{
+		productName: 'Night Bat Militech Drone',
+		productNumber: '71009',
+		paymentStatus: 'Due',
+		shipping: 'Pending',
+	},
+
+	{
+		productName: 'White Tiger Photo Drone',
+		productNumber: '32480',
+		paymentStatus: 'Refunded',
+		shipping: 'Declined',
+	},
+
+	{
+		productName: 'Night Panther Militech Drone',
+		productNumber: '11632',
+		paymentStatus: 'Due',
+		shipping: 'Pending',
+	},
+
+	{
+		productName: 'Night Vision Drone',
+		productNumber: '',
+		paymentStatus: 'Due',
+		shipping: 'Pending',
+	},
+
+	{
+		productName: 'Sandevistan',
+		productNumber: '11196',
+		paymentStatus: 'Due',
+		shipping: 'Pending',
+	},
+];
